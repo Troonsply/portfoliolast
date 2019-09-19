38c1166b22db15615057afd630109609a5219319
Катерина Буслова <ekaterinabuslova26@gmail.com>
2019-09-19 20:46:13 +0300

    edit

--- src/admin/components/pages/works.vue
+++ src/admin/components/pages/works.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
   .works-section
     .container
-      h1.page-title Блок "Мои работы"
-
+      h1.page-title Блок "Мои работы"yarn dev
 </template>
--- src/admin/router/index.js
+++ src/admin/router/index.js
@@ -6,7 +6,7 @@ import store from "@/store"
 
 Vue.use(VueRouter);
 
-const baseURL = "http://localhost:8000";
+const baseURL = "http://localhost:8080";
 const guard = axios.create({ baseURL });
 
 const router = new VueRouter({ routes });
